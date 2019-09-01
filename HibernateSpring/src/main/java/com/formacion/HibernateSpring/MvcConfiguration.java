cf50d598fb27d541cfcf9bb4f26d3105c1151a2e
jorge <jorge@DESKTOP-ELL64OG>
2019-09-01 13:55:03 +0200

    Formateo correcto de la clase

--- HibernateSpring/src/main/java/com/formacion/HibernateSpring/MvcConfiguration.java
+++ HibernateSpring/src/main/java/com/formacion/HibernateSpring/MvcConfiguration.java
@@ -7,18 +7,17 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
- 
+
 @Configuration
 @EnableWebMvc
 @ComponentScan
-public class MvcConfiguration implements WebMvcConfigurer
-{
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/view/");
-        resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
-        registry.viewResolver(resolver);
-    }
+public class MvcConfiguration implements WebMvcConfigurer {
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/view/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		registry.viewResolver(resolver);
+	}
 }
